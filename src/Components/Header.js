092047bf9724b1c5147bd5721f6b318e85ac3708
Damian Roiz <damianx989@gmail.com>
2022-07-09 15:38:23 -0400

    header and main components

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+    <header className="header">
+        <img src="./resources/troll-face.png" alt='logo'/>
+        <h1>Meme Generator</h1>
+        <p>React Course - Project 3</p>
+    </header>
+    )
+}
---
+++ src/Components/Meme.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function Meme() {
+    return (
+        <main>
+        <form>
+            <input 
+                type="text" 
+                placeholder="Top text"
+                className="form--input"
+            />
+            <input 
+                type="text" 
+                placeholder="bottom-text"
+                className="form--input"
+                />
+            <button className="form--button">Get a new meme image 🖼</button>
+        </form>
+        </main>
+    )
+}
+
