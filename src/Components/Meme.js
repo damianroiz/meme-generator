9419be47c0aedc757003da99184ae0fb8e15759a
Damian Roiz <damianx989@gmail.com>
2022-08-21 18:01:46 -0400

    making getMemes() async

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -29,9 +29,12 @@ const [meme, setMeme] = React.useState({
 const [allMemes, setAllMemes] = React.useState([])
 
 React.useEffect(function() {
-    fetch("https://api.imgflip.com/get_memes")
-    .then(res => res.json())
-    .then(data => setAllMemes(data.data.memes))
+    async function getMemes() {
+        const res = await fetch("https://api.imgflip.com/get_memes")
+        const data = await res.json()
+        setAllMemes(data.data.memes)
+    }
+    getMemes()
 }, [])    
 
 
